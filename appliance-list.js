13ccd61659f954d0f1979aacf77ad05d2d48d900
Ledgendairy1 <Ledgendairy1@gmail.com>
2015-04-24 17:32:22 -0400

    Create appliance-list.js

---
+++ appliance-list.js
@@ -0,0 +1,141 @@
+availableApplianceList = [
+	{
+		type: 'Light Bulb',
+		energyUsage: 60,
+		priority: 2,
+		schID: 'kitch'
+	},
+	{
+		type: 'Refrigerator',
+		energyUsage: 615,
+		priority: 1,
+		schID: 'kitch'
+	},
+	{
+		type: 'Microwave',
+		energyUsage: 1050,
+		priority: 3,
+		schID: 'kitch',
+		cycleDur : 3*60
+	},
+	{
+		type: 'Television',
+		energyUsage: 140,
+		priority: 2,
+		schID: 'lRoom',
+		cycleDur: 300*60
+	},
+	{
+		type: 'Ceiling fan',
+		energyUsage: 75,
+		priority: 3,
+		schID: 'lRoom'
+	},
+	{
+		type: 'Clothes Dryer',
+		energyUsage: 3600,
+		priority: 2,
+		schID: 'basement',
+		cycleDur: 60*60
+	},
+	{
+		type: 'Desktop Computer',
+		energyUsage: 200,
+		priority: 3,
+		schID: 'lRoom',
+		cycleDur: 300*60	
+	},
+	{
+		type: 'Dish Washer',
+		energyUsage: 1450,
+		priority: 2,
+		schID: 'kitch',
+		cycleDur: 120*60
+	},
+	{
+		type: 'Electric Mower',
+		energyUsage: 1500,
+		priority: 3,
+		schID: 'basement',
+		cycleDur: 60*60
+	},
+	{
+		type: 'Blow Dryer',
+		energyUsage: 1315,
+		priority: 3,
+		schID: 'bath',
+		cycleDur: 20*60
+	},
+	{
+		type: 'Air Conditioner',
+		energyUsage: 1080,
+		priority: 2,
+		schID: 'lRoom'
+	},
+	{
+		type: 'Iron',
+		energyUsage: 1095,
+		priority: 3,
+		schID: 'bath',
+		cycleDur: 10*60
+	},
+	{
+		type: 'Oven',
+		energyUsage: 2540,
+		priority: 1,
+		schID: 'kitch',
+		cycleDur: 60*60
+	},
+	{
+		type: 'Toaster',
+		energyUsage: 1080,
+		priority: 3,
+		schID: 'kitch',
+		cycleDur: 5*60
+	},
+	{
+		type: 'Vacuum Cleaner',
+		energyUsage: 1010,
+		priority: 3,
+		schID: 'lRoom',
+		cycleDur: 30*60
+	},
+	{
+		type: 'Washing Machine',
+		energyUsage: 1275,
+		priority: 1,
+		schID: 'basement',
+		cycleDur: 37*60
+	},
+	{
+		type: 'Coffee Machine',
+		energyUsage: 1035,
+		priority: 2,
+		schID: 'kitch',
+		cycleDur: 3*60
+	},
+	{
+		type: 'Water Heater',
+		energyUsage: 530,
+		priority: 1,
+		schID: 'basement'
+	},
+	{
+		type: 'Portable Heater',
+		energyUsage: 1560,
+		priority: 2,
+		schID: 'lRoom'
+	},
+	{
+		type: 'Electric Furnace',
+		energyUsage: 11830,
+		priority: 1,
+		schID: 'basement'
+	},
+	{
+		type: 'Central Air Conditioner',
+		energyUsage: 3830,
+		priority: 2,
+		schID: 'basement'
+	}
+]
